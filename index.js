ea2388cce507713952d162b2ef097d23118075ef
CALVINCE <calvince.ogwel@gmail.com>
2022-11-24 10:49:18 +0300

    completed assignment

--- index.js
+++ index.js
@@ -1,1 +1,14 @@
-// code your solution here
+function saturdayFun(activity = "roller-skate"){
+    return(`This Saturday, I want to ${activity}!`)
+}
+function mondayWork(activity = 'go to the office'){
+    return `This Monday, I will ${activity}.`
+}
+
+function wrapAdjective(adjective = "*") {
+    return function(message) {
+        return `You are ${adjective}${message}${adjective}!`
+    }
+}
+wrapAdjective(`*`)(`a hard worker`)(`*`)
+wrapAdjective(`||`)(`a dedicated programmer`)(`||`)
